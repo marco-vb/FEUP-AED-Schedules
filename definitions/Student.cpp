a25559d5a8849be283fafb1c9e2ddbb77730fe7c
pedro_lima <lima.pedro.0000@gmail.com>
2022-11-03 20:33:01 +0000

    Added Student docs

--- definitions/Student.cpp
+++ definitions/Student.cpp
@@ -26,10 +26,6 @@ set<pair<string, string>> Student::getClassesPerCourse() const {
     return classesPerCourse;
 }
 
-Schedule Student::getSchedule() const {
-    return schedule;
-}
-
 bool Student::addClassCourse(string const& classcode, string const& coursecode){
     pair<string, string> pair = {classcode, coursecode};
     if (classesPerCourse.find(pair) != classesPerCourse.end()) return false;
@@ -44,19 +40,4 @@ bool Student::removeClassCourse(string const& classcode, string const& coursecod
     return true;
 }
 
-bool Student::addSlot(Slot slot) {
-    return schedule.addSlot(std::move(slot));
-}
-
-void Student::printSchedule(ostream& out) const{
-    out << "Schedule for " << name << " (" << number << "):" << endl;
-    Schedule s = getSchedule();
-    s.printSchedule(out);
-}
-
-void Student::printSchedule(const string &filename) const{
-    ofstream out(filename);
-    printSchedule(out);
-    out.close();
-}
 
--- headers/Student.h
+++ headers/Student.h
@@ -6,26 +6,82 @@
 
 using namespace std;
 
-
+/**
+ * @brief A classe Student
+ * Esta classe é usada para representar um aluno
+ * Esta classe armazena o nome, o número e o horario do aluno
+ * Esta classe também armazena um conjunto de pares de strings
+ * Cada par de strings corresponde a uma turma e a uma unidade curricular
+ * Este conjunto é usado para armazenar as turmass por unidade curricular que o aluno está inscrito
+ */
 class Student {
     private:
+
+        /// O numero interno do aluno
         int number;
+
+        /// O nome do aluno
         string name;
+
+        /**
+         * @brief O conjunto de pares de strings (class, course)
+         * Este set é usado para armazenar as turmas por unidade curricular que o aluno está inscrito
+         * Cada par de strings contem a uma turma(class) e a uma unidade curricular(course
+         */
         set<pair<string, string>> classesPerCourse;
-        Schedule schedule;
 
     public:
+
+        /**
+         * @brief Construtor da classe Student
+         * Este construtor cria um aluno com o número dado
+         * @param number O número do aluno
+         * Complexidade Temporal: O(1)
+         */
         explicit Student(int number);
+
+        /**
+         * @brief Retorna o número do aluno
+         * @return O número do aluno
+         * Complexidade Temporal: O(1)
+         */
         int getNumber() const;
+
+        /**
+         * @brief Retorna o nome do aluno
+         * @return O nome do aluno
+         * Complexidade Temporal: O(1)
+         */
         string getName() const;
+
+        /**
+         * @brief Muda o nome do aluno
+         * @param name
+         */
         void setName(string name);
+
+        /**
+         * @brief Retorna o conjunto de pares de strings (class, course)
+         * @return O conjunto de pares de strings (class, course)
+         * Complexidade Temporal: O(1)
+         */
         set<pair<string, string>> getClassesPerCourse() const;
-        bool addSlot(Slot slot);
-        Schedule getSchedule() const;
+
+        /**
+         * @brief Adiciona um par de strings ao conjunto de pares de strings (class, course)
+         * @param class_ A turma
+         * @param course A unidade curricular
+         * Complexidade Temporal: O(log(N)) onde N é o número de pares de strings no conjunto
+         */
         bool addClassCourse(string const& classcode, string const& coursecode);
+
+        /**
+         * @brief Remove um par de strings do conjunto de pares de strings (class, course)
+         * @param class_ A turma
+         * @param course A unidade curricular
+         * Complexidade Temporal: O(log(N)) onde N é o número de pares de strings no conjunto
+         */
         bool removeClassCourse(string const& classcode, string const& coursecode);
-        void printSchedule(ostream& out = cout) const;    //prints through Schedule
-        void printSchedule(string const& filename) const;
 };
 
 
