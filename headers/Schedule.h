a006b00f02acd2af2d8af510f1822be1b1118693
pedro_lima <lima.pedro.0000@gmail.com>
2022-11-03 23:03:34 +0000

    Fixing docs notations

--- headers/Schedule.h
+++ headers/Schedule.h
@@ -7,10 +7,11 @@
 using namespace std;
 
 /**
- * @brief A classe Schedule
- * Esta classe é usada para representar um horariode uma semana
- * Este armazena um conjunto de slots
- * Um slot corresponde a uma aula
+ * @brief Classe Schedule
+ *
+ * Esta classe é usada para representar um horariode uma semana.\n
+ * Este armazena um conjunto de slots.\n
+ * Um slot corresponde a uma aula.\n
  * Pode ser usada para representar um horario de um aluno, de uma turma, de uma unidade curricular, etc.
  */
 class Schedule {
@@ -18,11 +19,12 @@ class Schedule {
     
         /**
          * @brief O vetor de sets de slots
-         * Este vetor é usado para armazenar os slots do horario
-         * É aqui que está armazenada a informação sobre o horario
-         * O vetor terá 6 posições, uma para cada dia da semana, exceto para o domingo
-         * Cada dia da semana é representado por um set de slots
-         * O tamanho vetor terá que ser constante e igual a 6
+         *
+         * Este vetor é usado para armazenar os slots do horario.\n
+         * É aqui que está armazenada a informação sobre o horario.\n
+         * O vetor terá 6 posições, uma para cada dia da semana, exceto para o domingo.\n
+         * Cada dia da semana é representado por um set de slots.\n
+         * O tamanho vetor terá que ser constante e igual a 6.\n
          */
         vector<set<Slot>> schedule;
         
@@ -36,81 +38,100 @@ class Schedule {
     
         /**
          * @brief Construtor da classe Schedule
-         * Este construtor cria um horario vazio
+         *
+         * Este construtor cria um horario vazio.\n
          * Complexidade Temporal: O(1)
          */
         Schedule();
         
         /**
          * @brief Retorna o vetor
-         * Função que retorna o vetor de sets de slots onde está armazenada a informação sobre o horario
-         * @return O vetor de sets de slots
+         *
+         * Função que retorna o vetor de sets de slots onde está armazenada a informação sobre o horario.\n
          * Complexidade Temporal: O(1)
+         *
+         * @return O vetor de sets de slots
          */
         vector<set<Slot>> getSchedule() const;
         
         /**
          * @brief Limpa o horario
-         * Função que limpa o horario, ou seja, usa a função clear() em cada set do vetor
-         * O(N) onde N é o número de aulas no horario
+         *
+         * Função que limpa o horario, ou seja, usa a função clear() em cada set do vetor.\n
+         * Complexidade Temporal: O(N) onde N é o número de aulas no horario
          */
         void clearSchedule();
         
         /**
          * @brief Adiciona um slot ao horario
-         * Função que adiciona um slot ao horario, adicionando-o ao set correspondente ao dia da semana, representado por uma posição no vetor
+         *
+         * Função que adiciona um slot ao horario, adicionando-o ao set correspondente ao dia da semana, representado por uma posição no vetor.\n
+         * Complexidade Temporal: O(logN) onde N é o número máximo de aulas de um dia
+         *
          * @param Slot O slot a adicionar
          * @return true se o slot foi adicionado, false caso contrário
-         * Complexidade Temporal: O(logN) onde N é o número máximo de aulas de um dia
          */
         bool addSlot(const Slot& slot);
         
         /**
          * @brief Verifica se existe colisão entre o slot dado e os slots do horario
-         * Função que verifica se existe colisão entre um slot e os slots já existentes no horario
-         * Para isso percorre todas as aulas do dia da semana e verifica se existe colisão
+         *
+         * Função que verifica se existe colisão entre um slot e os slots já existentes no horario.\n
+         * Para isso percorre todas as aulas do dia da semana e verifica se existe colisão.\n
+         * Complexidade Temporal: O(N) onde N é o número máximo de aulas de um dia
+         *
          * @param Slot O slot a verificar
          * @return true se existe colisão, false caso contrário
-         * Complexidade Temporal: O(N) onde N é o número máximo de aulas de um dia
          */
         bool checkForCollision(const Slot& slot);
         
         /**
          * @brief Operador []
-         * Função que retorna o set de slots correspondente ao dia da semana dado
+         *
+         * Função que retorna o set de slots correspondente ao dia da semana dado.\n
+         * Complexidade Temporal: O(1)
+         *
          * @param n O dia da semana em formato de inteiro
          * @return O set de slots correspondente ao dia da semana dado
-         * Complexidade Temporal: O(1)
          */
         set<Slot>& operator[] (int n);
         
         /**
          * @brief Operador []
-         * Função que retorna o set de slots correspondente ao dia da semana dado
+         *
+         * Função que retorna o set de slots correspondente ao dia da semana dado.\n
+         * Complexidade Temporal: O(1)
+         *
          * @param n O dia da semana em formato de string
          * @return O set de slots correspondente ao dia da semana dado
-         * Complexidade Temporal: O(1)
          */
         set<Slot>& operator[] (const string& day);
         
         /**
          * @brief Begin do horario
-         * @return  O iterador para o primeiro slot do horario, ou seja, o primeiro slot do primeiro dia da semana
+         *
          * Complexidade Temporal: O(1)
+         *
+         * @return  O iterador para o primeiro slot do horario, ou seja, o primeiro slot do primeiro dia da semana
          */
         set<Slot>::iterator begin();
         
         /**
          * @brief End do horario
-         * @return O iterador para o último slot do horario, ou seja, o último slot do último dia da semana
+         *
          * Complexidade Temporal: O(1)
+         *
+         * @return O iterador para o último slot do horario, ou seja, o último slot do último dia da semana
          */
         set<Slot>::iterator end();
         
         /**
          * @brief Inteiro para dia da semana
-         * Função que retorna uma string com o dia da semana correspondente ao inteiro dado
-         * Isto é feito usando o map numToWeekDay_
+         *
+         * Função que retorna uma string com o dia da semana correspondente ao inteiro dado.\n
+         * Isto é feito usando o map numToWeekDay_.\n
+         * Complexidade Temporal: O(1)
+         *
          * @param n 
          * @return String com o dia da semana
          */
@@ -118,8 +139,11 @@ class Schedule {
         
         /**
          * @brief Dia da semana para inteiro
-         * Função que retorna um inteiro com o dia da semana correspondente à string dada
-         * Isto é feito usando o map weekDayToNum_
+         *
+         * Função que retorna um inteiro com o dia da semana correspondente à string dada.\n
+         * Isto é feito usando o map weekDayToNum_,\n
+         * Complexidade Temporal: O(1)
+         *
          * @param day
          * @return Inteiro que representa o dia da semana
          */
--- headers/Student.h
+++ headers/Student.h
@@ -8,13 +8,13 @@
 using namespace std;
 
 /**
- * @brief A classe Student
+ * @brief Classe Student
  *
  * Esta classe é usada para representar um aluno.\n
  * Ela armazena o nome, o número e o horario do aluno.\n
  * Além disso armazena um conjunto de pares de strings.\n
  * Cada par de strings corresponde a uma turma e a uma unidade curricular.\n
- * Este conjunto é usado para armazenar as turmass por unidade curricular que o aluno está inscrito.\n
+ * Este conjunto é usado para armazenar as turmass por unidade curricular que o aluno está inscrito.
  */
 class Student {
     private:
@@ -27,8 +27,9 @@ class Student {
 
         /**
          * @brief O conjunto de pares de strings (class, course)
-         * Este set é usado para armazenar as turmas por unidade curricular que o aluno está inscrito
-         * Cada par de strings contem a uma turma(class) e a uma unidade curricular(course
+         *
+         * Este set é usado para armazenar as turmas por unidade curricular que o aluno está inscrito.\n
+         * Cada par de strings contem a uma turma(class) e a uma unidade curricular(course).
          */
         set<pair<string, string>> classesPerCourse;
 
@@ -36,55 +37,70 @@ class Student {
 
         /**
          * @brief Construtor da classe Student
-         * Este construtor cria um aluno com o número dado
-         * @param number O número do aluno
+         *
+         * Este construtor cria um aluno com o número dado.\n
          * Complexidade Temporal: O(1)
+         *
+         * @param number O número do aluno.\n
          */
         explicit Student(int number);
 
         /**
          * @brief Retorna o número do aluno
-         * @return O número do aluno
+         *
          * Complexidade Temporal: O(1)
+         *
+         * @return O número do aluno.\n
          */
         int getNumber() const;
 
         /**
          * @brief Retorna o nome do aluno
-         * @return O nome do aluno
+         *
          * Complexidade Temporal: O(1)
+         *
+         * @return O nome do aluno.\n
          */
         string getName() const;
 
         /**
          * @brief Muda o nome do aluno
-         * @param name
+         *
+         * Complexidade Temporal: O(1)
+         *
+         * @param name.\n
          */
         void setName(string name);
 
         /**
          * @brief Retorna o conjunto de pares de strings (class, course)
-         * @return O conjunto de pares de strings (class, course)
+         *
          * Complexidade Temporal: O(1)
+         *
+         * @return O conjunto de pares de strings (class, course)
          */
         set<pair<string, string>> getClassesPerCourse() const;
 
         /**
          * @brief Adiciona um par de strings ao conjunto de pares de strings (class, course)
+         *
+         * Complexidade Temporal: O(log(N)) onde N é o número de pares de strings no conjunto
+         *
          * @param class_ A turma
          * @param course A unidade curricular
          * @param courses O conjunto de todas unidades curriculares
          * @param classes O conjunto de todas turmas
-         * Complexidade Temporal: O(log(N)) onde N é o número de pares de strings no conjunto
          */
         bool addClassCourse(string const&, string const&, courseSet*, classSet*);
 
         /**
          * @brief Remove um par de strings do conjunto de pares de strings (class, course)
+         *
+         * Complexidade Temporal: O(log(N)) onde N é o número de pares de strings no conjunto
+         *
          * @param class_ A turma
          * @param course A unidade curricular
          * @param courses O conjunto de todas unidades curriculares
-         * Complexidade Temporal: O(log(N)) onde N é o número de pares de strings no conjunto
          */
         bool removeClassCourse(string const&, string const&, courseSet*);
 };
